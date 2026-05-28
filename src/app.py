38d5209a96fbd6316ae4f115d058b26b1bedc84a
Dudlwy <seandmashava@gmail.com>
2026-05-28 19:28:03 +0200

    moddified app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello from GitOps-managed Python App! modified", "status": "healthy"})
+    return jsonify({"message": "Hello from GitOps-managed Python App! modified again", "status": "healthy"})
 
 @app.route('/healthz')
 def healthz():
